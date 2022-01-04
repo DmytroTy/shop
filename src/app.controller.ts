099b6e994b528e568c983621469f08df3473c6f2
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-01-04 12:56:14 +0200

    Added LocalAuthGuard class

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,9 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor() {}
-
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
     return req.user;
