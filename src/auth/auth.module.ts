a0ae0aa76598e8059bfc0a15e2afc3d39a988275
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-01-06 11:43:55 +0200

    The environment is used for secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { BuyersModule } from '../buyers/buyers.module';
 import { LocalStrategy } from './strategies/local.strategy';
-import { jwtConstants } from './constants';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
     BuyersModule,
     PassportModule,
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60m' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '60m' },
+      }),
     }),
   ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
