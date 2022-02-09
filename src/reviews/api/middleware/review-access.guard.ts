2668778d11a3b1d3989884bdb885fd72cdb7f62e
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-02-09 17:24:23 +0200

    Added logging to review guard

--- src/reviews/api/middleware/review-access.guard.ts
+++ src/reviews/api/middleware/review-access.guard.ts
@@ -1,11 +1,13 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { LoggerWinston } from '../../../logger/logger-winston.service';
 import { Review } from '../../review.entity';
 
 @Injectable()
 export class ReviewAccessGuard implements CanActivate {
   constructor(
+    private readonly logger: LoggerWinston,
     @InjectRepository(Review)
     private reviewsRepository: Repository<Review>,
   ) {}
@@ -15,6 +17,10 @@ export class ReviewAccessGuard implements CanActivate {
   ): Promise<boolean> {
     const { params, user } = context.switchToHttp().getRequest();
     const review = await this.reviewsRepository.findOne(params.id, { where: { buyer: { id: user.userId } } });
+
+    if (!review) {
+      this.logger.warn(`User error: user with id = ${user.userId} haven't access to review with id = ${params.id}`, 'ReviewAccessGuard');
+    }
 
     return !!review;
   }
--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { LoggerModule } from '../logger/logger.module';
 import { ReviewsController } from './reviews.controller';
 import { Review } from './review.entity';
 import { ReviewsService } from './reviews.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review])],
+  imports: [
+    TypeOrmModule.forFeature([Review]),
+    LoggerModule,
+  ],
   controllers: [ReviewsController],
   providers: [ReviewsService],
   exports: [ReviewsService],
