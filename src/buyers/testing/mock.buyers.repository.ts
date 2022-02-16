3b58a74e1ed4a5dc6076957dc3190d0e775fd8d8
Dima Tyshchenko <dima.tyshchenko@ekreative.com>
2022-02-16 14:38:38 +0200

    Added tests for mail service

--- src/buyers/testing/mock.buyers.repository.ts
+++ src/buyers/testing/mock.buyers.repository.ts
@@ -1,20 +1,46 @@
 import { Buyer } from "../buyer.entity";
 
 export class MockBuyersRepository {
-  async findOne({ email }): Promise<Buyer> {
-    if (email === 'test@test.com')
+  async findOne(argument): Promise<Buyer> {
+    if (typeof argument === 'number') {
+      if (argument === 1) {
+        return {
+          id: 1,
+          username: 'test',
+          email: 'test@test.com',
+          password: '$2b$10$qJMKL4a8RkUB/hh2yK0ZFO0ZFzvlhEJDrd8FlCEeS1xnZIjvKjJku',
+        };
+      }
+      return null;
+    }
+
+    /* const { email, facebookId } = argument; */
+    if (argument.email === 'test@test.com') {
       return Object.assign({}, {
         id: 1,
         username: 'test',
         email: 'test@test.com',
         password: '$2b$10$qJMKL4a8RkUB/hh2yK0ZFO0ZFzvlhEJDrd8FlCEeS1xnZIjvKjJku',
       });
+    }
+
+    /* if (facebookId === '00000007') {
+      return {
+        id: 2,
+        username: 'test',
+        email: 'test2@test.com',
+        facebookId: '00000007',
+        password: '$2b$10$qJMKL4a8RkUB/hh2yK0ZFO0ZFzvlhEJDrd8FlCEeS1xnZIjvKjJku',
+      };
+    } */
 
     return null;
   }
 
+  /* async update(userId, updateBuyerDto) {} */
+
   save(createBuyerDto) {
-    createBuyerDto.id = 1;
+    createBuyerDto.id = 3;
     return createBuyerDto;
   }
 }
